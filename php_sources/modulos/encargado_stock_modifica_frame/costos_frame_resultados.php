4521628064a0ec6b222c17bb9189e1527089f87e
cam@IBMx3650m4 <cam@IBMx3650m4>
2024-04-30 18:42:24 -0300

    En la rama produccion20230922  Tu rama está actualizada con 'origin/produccion20230922'.
     Cambios a ser confirmados:
    	modificados:     php_sources/modulos/encargado_stock_modifica_frame/costos_frame_resultados.php

--- php_sources/modulos/encargado_stock_modifica_frame/costos_frame_resultados.php
+++ php_sources/modulos/encargado_stock_modifica_frame/costos_frame_resultados.php
@@ -82,7 +82,11 @@ while($row=mysql_fetch_array($result)){
 	// echo '<td><form action="stock_modifica.php" method="post" target="FrameMedio2"><input type="hidden" name="id_articulos" value="'.$row["id"].'" /> <input type="hidden" name="id_sucursal" value="'.$id_sucursal.'" /><input type="submit" name="stock" value="stock" /></form></td>';
 	echo '<td>';if($seg>0){ echo '<form action="aa.php" method="post" target="FrameMedio2"><input type="hidden" name="id_articulos" value="'.$row["id"].'" /> <input type="hidden" name="id_sucursal" value="'.$id_sucursal.'" /><input type="submit" name="seguir" value="seguir '.$seg.'" /></form>';} echo '</td>';
 	echo '<td><input type="text" name="stock'.$row["id"].'" value="'.$array_stock["stock"].'" size="3"></td>';
-	echo '<td><input type="text" name="fijo'.$row["id"].'" value="'.$array_stock["fijo"].'" size="3"></td>';
+	$fijo=($array_stock["stock"] - $array_stock["maximo"]);
+	if($fijo<0){
+		$fijo=0;
+	}
+	echo '<td><input type="text" name="fijo'.$row["id"].'" value="'.$fijo.'" size="3"></td>';
 	echo '<td><input type="text" name="minimo'.$row["id"].'" value="'.$array_stock["minimo"].'" size="3"></td>';
 	echo '<td><input type="text" name="maximo'.$row["id"].'" value="'.$array_stock["maximo"].'" size="3"></td>';
 	// echo '<td>'.$array_stock["minimo"].'</td>';
