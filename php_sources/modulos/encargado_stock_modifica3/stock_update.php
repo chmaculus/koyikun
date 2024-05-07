f37763cbec6290c5d02535c7c048f73465a1d59d
cam@IBMx3650m4 <cam@IBMx3650m4>
2024-05-07 15:35:29 -0300

    En la rama koyi20240506  Tu rama está actualizada con 'origin/koyi20240506'.
     Cambios a ser confirmados:
    	modificados:     php_sources/modulos/encargado_stock_modifica3/stock_update.php

--- php_sources/modulos/encargado_stock_modifica3/stock_update.php
+++ php_sources/modulos/encargado_stock_modifica3/stock_update.php
@@ -16,11 +16,15 @@ echo "<br><br>";
 
 verifica_tabla_stock( $_POST["id_articulos"], $id_sucursal );
 
-if($_POST["minimo"]<1){
+if($_POST["minimo"]<1 or $_POST["minimo"]==""){
 	$minimo=0;
+}else{
+	$minimo=$_POST["minimo"];
 }
-if($_POST["maximo"]<1){
+if($_POST["maximo"]<1 or $_POST["maximo"]==""){
 	$maximo=0;
+}else{
+	$maximo=$_POST["maximo"];
 }
 
 
