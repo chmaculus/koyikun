3a7e1a6edeeff09e5fcb240c35fbf24093d0cb76
cam@IBMx3650m4 <cam@IBMx3650m4>
2024-05-08 17:45:11 -0300

    En la rama acordate20240506  Tu rama está actualizada con 'origin/acordate20240506'.
     Cambios a ser confirmados:
    	nuevos archivos: php_sources/modulos/encargado_stock_modifica_noframe/export_stock.php
    	nuevos archivos: php_sources/modulos/encargado_stock_modifica_noframe/rotacion_export.inc.php

---
+++ php_sources/modulos/encargado_stock_modifica_noframe/export_stock.php
@@ -0,0 +1,199 @@
+<?php
+
+include_once("../../includes/connect.php");
+include_once("cabecera.inc.php");
+
+include_once("../../includes/funciones_varias.php");
+include_once("../../includes/funciones_costos.php");
+include_once("../../includes/funciones_precios.php");
+
+
+?>
+
+
+<body>
+
+
+
+<center>
+<titulo>Minimos y Maximos de stock</titulo>
+
+<?php
+
+$fecha=date("Y-n-d");
+
+
+
+
+$fecha=date("Y-n-d");
+$nombre_sucursal=nombre_sucursal($id_sucursal);
+
+
+echo "Sucursal: $nombre_sucursal <br>";
+
+$query='select * from articulos order by marca, 
+                clasificacion, 
+                subclasificacion, 
+                descripcion';
+
+$result=mysql_query($query)or die(mysql_error());
+$numrows=mysql_num_rows($result);
+#---------------------------------------------------------------
+
+echo '<center>';
+echo 'Sucursal: '.$nombre_sucursal.'<br>';
+
+echo '<br>Cantidad de articulos: '.$numrows.'<br>';
+
+?>
+
+<table class="t1">
+<tr>
+	<th>ID</th>
+	<th>Cod Int</th>
+	<th>Descripcion</th>
+	<th>Color</th>
+	<th>Contenido</th>
+	<th>Pesentacion</th>
+	<th>Clasificacion</th>
+	<th>Sub-clasificacion</th>
+	<th>cod barra</th>
+	<th>Stock</th>
+	<th>Minimo</th>
+	<th>Maximo</th>
+	<th>Mes</th>
+	<th>Tres</th>
+	<th>Seis</th>
+	<th>Nueve</th>
+	<th>Doce</th>
+</tr>
+<form action="stock_update2.php" method="post" enctype="multipart/form-data">
+
+
+<?php
+while($row=mysql_fetch_array($result)){
+	//$precio_costo=calcula_precio_costo( array_costo($row["id"]) );
+	// $seg=seg_stock($row["id"], 33);
+	$array_stock=stock_sucursal($row["id"],33);
+
+	$fijo=($array_stock["maximo"] - $array_stock["stock"] );
+	if($fijo<1){
+		$fijo=0;
+	}
+	
+	echo "<tr>";
+	echo '<td>'.$row["id"].'</td>';
+	echo '<td>'.$row["codigo_interno"].'</td>';
+	echo '<td>'.$row["descripcion"].'</td>';
+	echo '<td>'.$row["color"].'</td>';
+	echo '<td>'.$row["contenido"].'</td>';
+	echo '<td>'.$row["presentacion"].'</td>';
+	echo '<td>'.$row["clasificacion"].'</td>';
+	echo '<td>'.$row["subclasificacion"].'</td>';
+	echo '<td>'.$row["codigo_barra"].'</td>';
+	// echo '<td><form action="stock_modifica.php" method="post" target="FrameMedio2"><input type="hidden" name="id_articulos" value="'.$row["id"].'" /> <input type="hidden" name="id_sucursal" value="'.$id_sucursal.'" /><input type="submit" name="stock" value="stock" /></form></td>';
+	// if($fijo>0){
+	// 	echo '<td><font color="#007603" size="10px">'.$fijo.'</font></td>'.chr(10);
+	// }else{
+	// 	echo '<td><font color="#000000">'.$fijo.'</font></td>'.chr(10);
+	// }
+	
+	// echo '<td><input type="text" name="minimo'.$row["id"].'" value="'.$array_stock["minimo"].'" size="3"></td>';
+	// echo '<td><input type="text" name="maximo'.$row["id"].'" value="'.$array_stock["maximo"].'" size="3"></td>';
+
+	echo '<td>'.$array_stock["stock"].'</td>';
+	echo '<td>'.$array_stock["minimo"].'</td>';
+	echo '<td>'.$array_stock["maximo"].'</td>';
+	include("rotacion_export.inc.php");
+	// echo '<td>'.$array_stock["fecha"].'</td>';
+	// echo '<td>'.$array_stock["hora"].'</td>';
+
+
+	echo "</tr>".chr(13);
+}
+
+
+
+echo "</table>";
+echo '<input type="hidden" name="id_sucursal" value="'.$id_sucursal.'">';
+echo '<input type="hidden" name="query" value="'.base64_encode($query).'">';
+echo '<input type="submit" name="ACEPTAR" value="ACEPTAR">';
+echo "</form>";
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------
+function aa1($id_articulo, $id_sucursal){
+	$q='select stock, maximo from stock where id_articulo="'.$id_articulo.'" and id_sucursal="'.$id_sucursal.'"';
+	//echo $q."<br>";
+	$r=mysql_query($q);
+	$rows=mysql_num_rows($r);
+	
+	if($rows==1){
+		$row=mysql_fetch_array($r);
+		$reponer = ($row["maximo"] - $row["stock"]) ;
+		if ($reponer<0){
+			$reponer=0;
+		}
+		return $reponer;
+		 
+	}
+}
+#---------------------------------------------------------------------------------------------
+
+
+
+
+
+
+#-----------------------------------------------------------------
+function stock_sucursal($id_articulo,$id_sucursal){
+	$query='select * from stock where 	id_articulo="'.$id_articulo.'" and id_sucursal="'.$id_sucursal.'"';
+	$res=mysql_query($query) or die(mysql_error()." ".$SCRIPT_NAME);
+	$rows=mysql_num_rows($res);
+	if($rows==1){
+		$array=mysql_fetch_array($res);
+		$array["rows"]=$rows;
+		return $array;		  
+	}
+	if($rows<1){
+		$array["stock"]="0";
+		$array["maximo"]="0";
+		$array["minimo"]="0";
+		$array["id_sucursal"]=$id_sucursal;
+		$array["rows"]=0;
+		return $array;		
+	}
+}
+#-----------------------------------------------------------------
+
+
+#-----------------------------------------------------------------
+function seg_stock($id_articulo, $id_sucursal){
+	//$q='select id from seguimiento_stock where id_articulo="'.$id_articulo.'" and (origen="'.$id_sucursal.'" or destino="'.$id_sucursal.'")';
+	$q='select * from seguimiento_stock where id_articulo="'.$id_articulo.'" and 	(
+																																(	(origen="'.$id_sucursal.'" and tipo="EN") or  
+																																	(destino="'.$id_sucursal.'" and tipo="RE") or
+																																	(origen="'.$id_sucursal.'" and tipo="VE") or
+																																	(origen="'.$id_sucursal.'" and tipo="MD")
+																													 			)  
+																													 		)   order by fecha, hora';
+
+	$res=mysql_query($q);
+	$rows=mysql_num_rows($res);
+	return $rows;
+}
+#-----------------------------------------------------------------
+
+
+
+?>
+</center>
+</body>
+</html>
---
+++ php_sources/modulos/encargado_stock_modifica_noframe/rotacion_export.inc.php
@@ -0,0 +1,32 @@
+<?php
+$q='select * from ventas_estadistica where id_articulo='.$row["id"];
+$res=mysql_query($q);
+if(mysql_error()){
+	echo $q."<br>";
+	echo mysql_error();
+}
+// echo "<td> $q </td>";
+$array_estadistica=mysql_fetch_array($res);
+
+echo '<td>'.round($array_estadistica["mes"],0).'</td>';
+		
+echo '<td>'.round($array_estadistica["tres"],0).'</td>';
+
+echo '<td>'.round($array_estadistica["seis"],0).'</td>';
+
+echo '<td>'.round($array_estadistica["nueve"],0).'</td>';
+
+echo '<td>'.round($array_estadistica["doce"],0).'</td>';
+
+// echo "<tr><td>Min</td>";
+// echo '<td>'.round($array_estadistica["tres"],0).'</td>';
+// echo "</tr>";
+
+// echo "<tr><td>Max</td>";
+// echo '<td>'.round($maximo,0).'</td>';
+// echo "</tr>";
+
+// echo '<tr class="special1"><td>Stk</td>';
+// echo '<td>'.round($stock["stock"],0).'</td>';
+// echo "</tr>";
+?>
